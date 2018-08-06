0fa37dce8dca747d9a2cadee94410d7cc9ef9e0b
Nickita Manukol <nikitamanukol23274@gmail.com>
2018-08-06 22:16:24 +0300

    ДОБАВЛЯЮ СТРЕЛЬБУ
    йцуцйуйу

---
+++ My_Dream_2D/Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    float speed = 20;
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+    }
+}
